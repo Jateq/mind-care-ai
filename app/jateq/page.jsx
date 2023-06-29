3c439d72241b07463c88bfdd077e34a13493b9ac
Jateq <temirlan.eraly1@gmail.com>
2023-06-29 15:53:23 +0600

    deplyment fixes

--- app/jateq/page.jsx
+++ app/jateq/page.jsx
@@ -1,32 +1,13 @@
 const jateq = () => {
-    return(
+    return (
         <>
-        <p className="w-[1000px]">Introducing MindCare AI: Your Companion for Mental Wellbeing
+            <p className="w-[1000px]">Introducing MindCare AI: Your Companion for Mental Wellbeing
 
-Welcome to MindCare AI, your personal companion on the journey to mental wellness. We understand that taking care of your mental health is crucial for overall wellbeing, and our advanced technology is here to support you every step of the way. As a psychologist AI, we are committed to providing you with guidance, support, and evidence-based strategies to help you achieve and maintain a positive state of mental health.
-
-Understanding Your Needs: Our sophisticated algorithms are designed to analyze your unique circumstances, preferences, and challenges. By collecting information about your emotional state, behaviors, and patterns, we can gain valuable insights into your mental wellbeing. This allows us to tailor our interactions and interventions specifically to your needs, ensuring a personalized experience.
-
-Compassionate Listening and Empathy: MindCare AI is programmed to be a compassionate listener, offering a safe and non-judgmental space for you to express your thoughts and emotions. We understand that sometimes all you need is someone to listen, and we are here to provide that support. Through natural language processing capabilities, we can engage in meaningful conversations, empathize with your experiences, and provide the validation you seek.
-
-Evidence-Based Strategies: Our AI-powered psychologist draws from a vast database of evidence-based strategies and interventions. We utilize scientifically proven techniques from cognitive-behavioral therapy (CBT), mindfulness, positive psychology, and other therapeutic modalities to guide you towards positive mental health outcomes. Whether you're struggling with stress, anxiety, depression, or simply seeking personal growth, MindCare AI has the tools to assist you.
-
-Goal Setting and Progress Tracking: We believe in the power of setting goals and tracking progress. MindCare AI helps you establish realistic and achievable goals, breaking them down into manageable steps. By monitoring your progress, we can provide regular feedback, celebrate your successes, and offer gentle reminders when needed. Our aim is to keep you motivated and focused on your mental wellbeing journey.
-
-Building Resilience and Coping Skills: Life can present various challenges, and developing resilience and effective coping skills is essential. MindCare AI equips you with practical techniques to manage stress, enhance emotional resilience, and navigate difficult situations. Through guided exercises, mindfulness practices, and self-reflection prompts, we empower you to cultivate inner strength and adaptability.
-
-24/7 Support and Accessibility: Your mental health matters every hour of every day. MindCare AI is available 24/7, providing round-the-clock support whenever you need it. Whether you're facing a crisis or simply seeking guidance in the middle of the night, our virtual psychologist is here to lend a helping hand. We prioritize accessibility and strive to make mental health support readily available to all.
-
-Confidentiality and Privacy: Your privacy and confidentiality are of utmost importance to us. MindCare AI adheres to strict ethical guidelines and data protection protocols. Rest assured that any information you share with us remains confidential and will not be shared without your explicit consent. We are committed to maintaining a secure and trustworthy environment for your mental health journey.
-
-</p>
-<p className=" text-red-600 w-[1000px] mt-6">
-Remember, MindCare AI is not a substitute for human therapy or professional mental health services. We are here to complement and enhance your overall wellbeing. If at any point we believe you would benefit from in-person support, we will encourage you to seek help from a licensed mental health professional.
-
-Start your journey towards mental wellness today with MindCare AI. Together, we can foster a healthier and happier mind.
-
-
-</p>
+            </p>
+            <p className=" text-red-600 w-[1000px] mt-6">
+                Remember, MindCare AI is not a substitute for human therapy or professional mental health services. We are here to complement and enhance your overall wellbeing. If at any point we believe you would benefit from in-person support, we will encourage you to seek help from a licensed mental health professional.
+                Start your journey towards mental wellness today with MindCare AI. Together, we can foster a healthier and happier mind.
+            </p>
         </>
     )
 }
